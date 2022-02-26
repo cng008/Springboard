50c4d2b9353a0ad0098dee6e71bca6b8fa6b1f67
Christien Ng <ngchristien@gmail.com>
2022-02-25 23:07:50 -0800

    create handler for add pet form, add validation

--- 24.1_flask-adopt/app.py
+++ 24.1_flask-adopt/app.py
@@ -25,3 +25,27 @@ def show_list():
     pets = Pet.query.all()
     return render_template("listing.html", pets=pets)
 
+
+@app.route('/add', methods=["GET", "POST"])
+def add_pet():
+    """Add pet form; handle adding."""
+
+    form = AddPetForm()
+    # species = db.session.query(Pet.species)
+    # form.species.choices = species
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        new_pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash((f"Added {name}"))
+        return redirect('/')
+    else:
+        return render_template('/new.html', form=form)
--- 24.1_flask-adopt/forms.py
+++ 24.1_flask-adopt/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, IntegerField, BooleanField
+from wtforms.validators import InputRequired, Optional, URL, NumberRange
 
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
-    name = StringField('Name')
-    species = SelectField('Species')
-    photo_url = StringField('Photo URL')
-    age = IntegerField('Age')
-    notes = StringField('Notes')
-    available = BooleanField('Available?')
+    name = StringField('Name', validators=[InputRequired()])
+    species = SelectField('Species', choices=[('dg','dog'), ('ct', 'cat'), ('tt', 'turtle')], validators=[InputRequired()])
+    photo_url = StringField('Photo URL', validators=[Optional(), URL()])
+    age = IntegerField('Age', validators=[Optional(), NumberRange(min=0, max=30)])
+    notes = StringField('Notes', validators=[Optional()])
+    available = BooleanField('Available?', validators=[InputRequired()], default="checked")
